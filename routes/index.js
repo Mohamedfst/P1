5b3615e9bafdf79506d97880f3f0d5e787536ac4
Mohamedfst <keita.momo1@gmail.com>
2021-11-05 08:35:34 -0700

    Added test, and the required codes to add a message to the application.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../bin/settings.js';
-import { messagesPage } from './messages.js';
+import { messagesPage, addMessage } from './messages.js';
 
 const indexRouter = express.Router();
 /* GET home page. */
@@ -8,5 +8,7 @@ indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironme
 
 /* add the get messages endpoint. */
 indexRouter.get('/messages', messagesPage);
+
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
